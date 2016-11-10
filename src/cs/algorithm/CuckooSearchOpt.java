eb3c7d3ac3680ca93194e678cee9e67234fdf316
Duduf15 <cyril.dufay@gmail.com>
2016-11-10 16:27:51 +0100

    adapatation cs

--- src/cs/algorithm/CuckooSearchOpt.java
+++ src/cs/algorithm/CuckooSearchOpt.java
@@ -1,8 +1,10 @@
 package cs.algorithm;
 
 
+import java.util.ArrayList;
 import java.util.Random;
 
+import cmaes2.fitness.IObjectiveFunction;
 import imss.ConvertArrays;
 
 
@@ -10,61 +12,38 @@ public class CuckooSearchOpt extends OptimizationAlgorithm {
 
     private CSSolutionSet solutions;
     private int N_NESTS;					//number of nests (solutions)
-    private int N_OPTIMIZATIONS;			//number of generations
     private double ABANDON_PROBABILITY;		//percentage of worst solutions discarded
-    private int MAX_RANDOM_ATTEMPTS;			//maximum attempts to create a new random solution
+    private int MAX_RANDOM_ATTEMPTS;
+    private final int numVar;
+    private OptimizationProblem optProb;//maximum attempts to create a new random solution
+    private Random rand;
+    private int numRuns;
     
-    public CuckooSearchOpt( ) {
-    	N_NESTS = 15;
-    	//56000
-		N_OPTIMIZATIONS = 100;
-		ABANDON_PROBABILITY = 0.25;
-		MAX_RANDOM_ATTEMPTS = 1000;
+    public CuckooSearchOpt() {
+    	
+		numVar=0;
+		
     }
     
-    public CuckooSearchOpt(int nNests, int nOptimisations, double abandonProb, int maxRandomAttempts ) {
-    	this.N_NESTS = nNests;
-		this.N_OPTIMIZATIONS = nOptimisations;
-		this.ABANDON_PROBABILITY= abandonProb;
-		MAX_RANDOM_ATTEMPTS = maxRandomAttempts;
+    
+    public CuckooSearchOpt(int taillePop, OptimizationProblem probleme) {
+    	this.N_NESTS = taillePop;
+    	this.optProb=probleme;
+    	ABANDON_PROBABILITY = 0.25;
+		MAX_RANDOM_ATTEMPTS = 1000;
+		numVar=probleme.getNumVar();
+		rand = new Random();
+		numRuns = 0;
     }
    
-	public void solve(OptimizationProblem optProb) {
-		/* 
-		 * Objective function 
-		 * Generate an initial population of host nests; 
-		 * While (t<MaxGeneration) or (stop criterion)
-		 *    Get a cuckoo randomly (say, i) and replace its solution by performing Lévy flights;
-		 *    Evaluate its quality/fitness F_i
-		 *          [For maximization, -F_i];
-		 *    Choose a nest among n (say, j) randomly;
-		 *    if (F_i > F_j),
-		 *           Replace j by the new solution;
-		 *    end if
-		 *    A fraction (P_a) of the worse nests are abandoned and new ones are built;
-		 *    Keep the best solutions/nests;
-		 *    Rank the solutions/nests and find the current best;
-		 *    Pass the current best solutions to the next generation;
-		 * end while
-		*/
-
-		//for collecting data - delete afterwards
-		//fitnesses = new ArrayList<Double>(N_OPTIMIZATIONS/NUM_DATA+1);
-		
-		//nombre de variables utilisées dans le probleme
-		int NUM_VAR = optProb.getNumVar();
-		
+	public void solve(double[][] population) {
 		//creation d'une liste de solution avec un certains nombre de nids (N_NESTS)
-		solutions = new CSSolutionSet(N_NESTS, NUM_VAR);
+		solutions = new CSSolutionSet(N_NESTS, numVar);
 				
 		//initialisation aléatoire des solution
 		solutions.initializeWithRandomSols(optProb);
 		
-		Random rand = new Random();
-
-		int numRuns = 0;
 		int tries;
-		for (int t = 0; t < N_OPTIMIZATIONS; t++) {
 			
 			//retourne une solution aléatoire parmis la liste des solutions du probleme
 			CSSolution i = solutions.getRandSol();
@@ -100,30 +79,41 @@ public class CuckooSearchOpt extends OptimizationAlgorithm {
 		    
 		    numRuns++;
 		    solutions.setNumRuns(numRuns);
-		    
-		    //for collecting data - TODO: delete afterwards
-		    /*
-		    if((t+1)%(N_OPTIMIZATIONS/NUM_DATA)==0) {
-		    	fitnesses.add(new Double(solutions.getMostFitSolution(optProb).getFitness()));
-		    }*/
-		    System.out.println("nb var : "+solutions.getSolutions().get(1).getVars().size());
-		    System.out.println("nb solution : "+solutions.getSolutions().size());
+	}
+	
+	public double[][] listToArray(){
+		 ArrayList<CSSolution> listeSolutions=solutions.getSolutions();
+		 double[][] tabSolutions=new double[listeSolutions.size()][numVar];
+		 
+			for(int i=0;i<listeSolutions.size();i++){
+				ArrayList<Double> vars=listeSolutions.get(i).getVars();
+				for(int j=0;j<vars.size();j++){
+					tabSolutions[i][j]=vars.get(j);
+				}
+			}
+			return tabSolutions;
 		}
-		
+	
+	public void arrayToList(double[][] tabSolutions){
+		for(int i=0;i<tabSolutions.length;i++){
+			for(int j=0;j<tabSolutions[i].length;j++){
+				solutions.getSolutions().get(i).getVars().set(j, tabSolutions[i][j]);
+			}
+		}
 	}
-	public double[][] solve(double[][] population){
-		
-		int num_var=population[0].length;
-		
-		
-		
-		
-		
-		return null;
-	}
+	
+	
+	
+
 	// TODO: prevent returning null. Instead throw an exception. 
 	public CSSolutionSet getSolutions(OptimizationProblem optProb) {
 	    solutions.sortByFitness(optProb);
         return solutions;
     }
+
+	@Override
+	public void solve(OptimizationProblem optProb) {
+		// TODO Auto-generated method stub
+		
+	}
 }
